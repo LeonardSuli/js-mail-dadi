ee73eeb449c11f5f66f9757288358bf7f0a56dc0
LeonardSuli <leonardsuli1991@gmail.com>
2023-11-27 20:19:22 +0100

    add es mail

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+
+//Mail
+//Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+//lista email
+const lista_email = ['leo@gmail.it', 'fabio@gmail.it', 'marco@gmail.it']
+
+//chiedo all'utente la sua email
+const email = prompt('Dimmi la tua E-mail')
+
+for (let i = 0; i < lista_email.length; i++) {
+    const element = lista_email[i];
+    console.log(element) 
+}
+
+if(lista_email === element){
+    alert('Benvenuto')
+}else{
+    alert('Accesso negato')
+}
